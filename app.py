1d80aa78e3c55cbff017cf87f32ce5e971372671
shihar <shiharbinsideeque73@gmail.com>
2021-08-07 16:19:55 +0530

    requirements.txt added

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import *
 import requests
 import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+import pickle
+model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
 
 app = Flask(__name__)
 app.secret_key ="helloo"
@@ -9,13 +13,13 @@ app.secret_key ="helloo"
 def Home():
     return render_template('index.html')
 
-
+standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
 def predict():
     Year = int(request.form['Year'])
     Present_Price=float(request.form['Present_Price'])
     Kms_Driven=int(request.form['Kms_Driven'])
-    Kms_Driven2=np.log(Kms_Driven)
+    # Kms_Driven2=np.log(Kms_Driven)
     Owner=int(request.form['Owner'])
     Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
     if(Fuel_Type_Petrol=='Petrol'):
@@ -27,7 +31,7 @@ def predict():
     else:
         Fuel_Type_Petrol=0
         Fuel_Type_Diesel=0
-    Year=2020-Year
+    Year=2021-Year
     Seller_Type_Individual=request.form['Seller_Type_Individual']
     if(Seller_Type_Individual=='Individual'):
         Seller_Type_Individual=1
@@ -39,9 +43,17 @@ def predict():
     else:
         Transmission_Mannual=0
     
-    print("Present_Price",Present_Price,"Kms_Driven2",Kms_Driven2,"Owner",Owner,"Year",Year,"Fuel_Type_Diesel",Fuel_Type_Diesel,"Fuel_Type_Petrol",Fuel_Type_Petrol,"Seller_Type_Individual",Seller_Type_Individual,"Transmission_Mannual",Transmission_Mannual)
-    flash("dgdsfgsdf")
-    return redirect(url_for("Home"))
+    print("Present_Price",Present_Price,"Kms_Driven2",Kms_Driven,"Owner",Owner,"Year",Year,"Fuel_Type_Diesel",Fuel_Type_Diesel,"Fuel_Type_Petrol",Fuel_Type_Petrol,"Seller_Type_Individual",Seller_Type_Individual,"Transmission_Mannual",Transmission_Mannual)
+    prediction=model.predict([[Present_Price,Kms_Driven,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+    output=round(prediction[0],2)
+    if output<0:
+        prediction_text="Sorry you cannot sell this car"
+        flash(prediction_text)
+        return redirect(url_for("Home"))
+    else:
+        prediction_text="You Can Sell The Car at {}".format(output)
+        flash(prediction_text)
+        return redirect(url_for("Home"))
 
 
 
